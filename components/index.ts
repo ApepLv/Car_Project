655d808b2080b927a3c96544718e9eb0c156c20b
Kristaps Puriņš <kristaps.purinsh@gmail.com>
2023-09-16 08:39:13 +0300

    Initial commit for Car_Project

---
+++ components/index.ts
@@ -0,0 +1,10 @@
+import CustomButton from "./CustomButton";
+import Footer from "./Footer";
+import Hero from "./Hero";
+import Navbar from "./Navbar";
+
+export {
+    CustomButton, Footer, Hero,
+    Navbar
+};
+

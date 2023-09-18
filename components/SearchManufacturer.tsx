a308171d4e84ea9c6a7adbaedfc1b05ba2dd7ab2
Kristaps Puriņš <kristaps.purinsh@gmail.com>
2023-09-18 16:29:34 +0300

    forth commit

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -22,7 +22,7 @@ const SearchManufacturer = ({manufacturer,setManufacturer} : SearchManufacturerP
 
   return (
     <div className="search-manufacturer">
-        <Combobox>
+        <Combobox value={manufacturer} onChange={setManufacturer}>
             <div className="relative w-full">
               <Combobox.Button className="absoulute top-
               [14px]">
@@ -49,14 +49,47 @@ const SearchManufacturer = ({manufacturer,setManufacturer} : SearchManufacturerP
           afterLeave={() => SetQuery ('')}
           >
             <Combobox.Options>
+              {
+                filteredManufacturers.map((item) => (
+                <Combobox.Option
+                  key={item}
+                  className={({active}) => `
+                  relative search-manufacturer__option
+                  ${active ? `bg-primary-blue
+                  text-white` : `text-gray-900`}
+                `}
+                value={item}
+                >
 
+                {({ selected, active}) => (
+                <>
+                <span
+                          className={`block truncate ${
+                            selected ? 'font-medium' : 'font-normal'
+                          }`}
+                        >
+                          {item}
+                        </span>
+                        {selected ? (
+                          <span
+                          className={`absolute inset-y-0 left-0 flex items-center pl-3 ${
+                            active ? 'text-white' : 'text-teal-600'
+                          }`}
+                          >
+                          </span>
+                        ) : null}
+                </>
+                )}
+                </Combobox.Option>
+                )
+                )}
             </Combobox.Options>
-
           </Transition>
 
             </div>
         </Combobox>
     </div>
+
   )
 }
 

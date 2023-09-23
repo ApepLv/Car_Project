1731a1391fbce7516073c30f9bb56e9b0a5aad7f
ApepLv <purins.kristaps@inbox.lv>
2023-09-23 21:55:13 +0300

    test

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default async function Home() {
      max-width" id="discover">
         <div className="home__text-container">
           <h1 className="text-4x1 font-extrabold">Car Catalog</h1>
-          <p>Explore the cars you might like</p>
+          <p>Explore the cars you might like!</p>
         </div>
         <div className="home__filters">
           <SearchBar/>
